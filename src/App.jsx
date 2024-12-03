96390e688383ef33865df8ed00c3a8ba4ead3210
JayAhir17 <158028334+JayAhir17@users.noreply.github.com>
2024-12-03 10:15:16 +0530

    redux applying

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { getUsers } from './redux/actions/userActions';
+import UserForm from './component/UserForm';
+import UserTable from './component/UserTable';
+import { connect } from 'react-redux'
+
+const App = () => {
+  const dispatch = useDispatch();
+  const [selectedUser, setSelectedUser] = useState(null);
+
+  useEffect(() => {
+    dispatch(getUsers());
+  }, [dispatch]);
+
+  return (
+    <div>
+      <h1>Redux Crud</h1>
+      <UserForm selectedUser={selectedUser} setSelectedUser={setSelectedUser} />
+      <UserTable setSelectedUser={setSelectedUser} />
+    </div>
+  );
+};
+
+export default App;
